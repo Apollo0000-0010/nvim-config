e78daafb47b2ea42ca05533c1102663a591442b1
Michael Tamplen <michael.tamplen@atheneum.ai>
2024-03-15 07:55:19 -0700

    remap netrw

--- lua/apollo/remap.lua
+++ lua/apollo/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>no", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>nv", vim.cmd.Vex)
 
 vim.keymap.set("n", "<leader>wh", "<C-w><LEFT>")
 vim.keymap.set("n", "<leader>wl", "<C-w><RIGHT>")
