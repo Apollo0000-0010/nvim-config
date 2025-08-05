d90d47ec1334c9d0d45fdc4d0da72a73f4c60d5c
Michael T <apollo0dev0@gmail.com>
2025-08-05 07:44:26 -0700

    feat: update

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -13,3 +13,9 @@ vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.termguicolors = true
+
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
+vim.o.background="dark"
+vim.cmd([[colorscheme gruvbox]])
